15423c52fbd373d84c3100e4ece81ec758c08a6c
Manish Gupta <manigu@microsoft.com>
2018-07-19 21:23:39 -0700

    Got the nibs working, the main thing has some issues

--- puzzleEnv.py
+++ puzzleEnv.py
@@ -3,6 +3,10 @@ from PIL import Image
 import numpy as np
 import random
 import math
+
+# y = im.shape[0]
+# x = im.shape[1]
+
 class EdgeShape(Enum):
 	STRAIGHT = 0
 	IN = 1
@@ -39,20 +43,20 @@ class Direction(Enum):
 class PuzzlePiece:
 	NIB_PERCENT = 10 / 100
 
-	def init(self, imgData, coords):
-		self.imgData = imgData # tbd
+	def __init__(self, coords):
+		# self.imgData = imgData # tbd
 		self.coords = coords
 		self.edgeGeometry = 0 # L, D, R, U (2 bits per direction totals 1 byte)
 
 	def getEdgeGeometry(self, direction):
-		if direction == UP:
-			return EdgeShape(int(bin(edgeGeometry)[2:][6:8]), 2)
-		elif direction == RIGHT:
-			return EdgeShape(int(bin(edgeGeometry)[2:][4:6]), 2)
-		elif direction == DOWN:
-			return EdgeShape(int(bin(edgeGeometry)[2:][2:4]), 2)
-		elif direction == LEFT:
-			return EdgeShape(int(bin(edgeGeometry)[2:][0:2]), 2)
+		if direction == Direction.UP:
+			return EdgeShape(int(bin(self.edgeGeometry)[2:][6:8], 2))
+		elif direction == Direction.RIGHT:
+			return EdgeShape(int(bin(self.edgeGeometry)[2:][4:6], 2))
+		elif direction == Direction.DOWN:
+			return EdgeShape(int(bin(self.edgeGeometry)[2:][2:4], 2))
+		elif direction == Direction.LEFT:
+			return EdgeShape(int(bin(self.edgeGeometry)[2:][0:2], 2))
 
 	def setEdgeGeometry(self, direction, edgeShape):
 		if direction == UP:
@@ -74,7 +78,7 @@ class Puzzle:
 		self.yNumPieces = yNumPieces
 
 	def createEdge(self, piece0Coords, piece1Coords, p0EdgeDirection):
-		if (piecesArray[piece0Coords].getEdgeGeometry(p0EdgeDirection) != EdgeShape.STRAIGHT):
+		if (self.piecesArray[piece0Coords[0]][piece0Coords[1]].getEdgeGeometry(p0EdgeDirection) != EdgeShape.STRAIGHT):
 			return
 
 		p1EdgeDirection = Direction.GetComplement(p0EdgeDirection)
@@ -82,25 +86,25 @@ class Puzzle:
 		p0EdgeShape = EdgeShape(random.randint(1,2))
 		p1EdgeShape = EdgeShape.GetComplement(p0EdgeShape)
 
-		piecesArray[piece0Coords].setEdgeGeometry(p0EdgeDirection, p0EdgeShape)
-		piecesArray[piece1Coords].setEdgeGeometry(p1EdgeDirection, p1EdgeShape)
+		piecesArray[piece0Coords[0]][piece0Coords[1]].setEdgeGeometry(p0EdgeDirection, p0EdgeShape)
+		piecesArray[piece1Coords[0]][piece1Coords[1]].setEdgeGeometry(p1EdgeDirection, p1EdgeShape)
 
 	def generatePuzzleGeometry(self, xNumPieces, yNumPieces):
-		self.piecesArray = numpy.zeros(shape=(xNumPieces,yNumPieces))
-		for x in xNumPieces:
-			for y in yNumPieces:
-				piecesArray[x, y].coords = (x, y)
+		self.piecesArray = [[None for i in range(xNumPieces)] for i in range(yNumPieces)]
+		for x in range(xNumPieces):
+			for y in range(yNumPieces):
+				self.piecesArray[y][x] = PuzzlePiece((y, x))
 				if (x < (xNumPieces - 1)):
-					createEdge((x,y), (x+1,y), Direction.RIGHT)
+					self.createEdge((y,x), (y,x+1), Direction.RIGHT)
 
 				if (x > 0):
-					createEdge((x,y), (x-1,y), Direction.LEFT)
+					self.createEdge((y,x), (y,x-1), Direction.LEFT)
 
 				if (y < (xNumPieces - 1)):
-					createEdge((x,y), (x,y+1), Direction.DOWN)
+					self.createEdge((y,x), (y+1,x), Direction.DOWN)
 
 				if (y > 0):
-					createEdge((x,y), (x,y-1), Direction.UP)
+					self.createEdge((y,x), (y-1,x), Direction.UP)
 
 
 	def addOuterNib(self, singlePieceImgData, singlePieceDimensions, coords, nibHeight):
@@ -110,50 +114,44 @@ class Puzzle:
 		# Use square nibs for now
 		nibWidth = nibHeight
 
-		print(singlePieceImgData.shape)
-
-		# upper box for an out nib
-
-		# (0,0) (nibHeight, 0)
-		# (0, ((singlePieceHeight - nibWidth) / 2) (nibHeight, (0, ((singlePieceHeight - nibWidth) / 2))
-		
-		boxHeight = math.floor((singlePieceHeight - nibWidth) / 2)
-		singlePieceImgData[0:nibHeight, 0:boxHeight] = (0, 0, 0)
-		singlePieceImgData[0:nibHeight, singlePieceHeight - boxHeight : singlePieceHeight ] = (0, 0, 0)
-
-
-		# lower box for an out nib
-		# (0, ((singlePieceHeight + nibWidth) / 2) ), (nibHeight, ((singlePieceHeight + nibWidth) / 2) )
-		# (0, singplePiecehight) (nibHeight, singlePieceheight)
-
-		# singlePieceImgData[0:nibHeight, ((singlePieceHeight + nibWidth) / 2) : singlePieceHeight ] = (0, 0, 0)
-		# singlePieceImgData[0:nibHeight, ((singlePieceHeight + nibWidth) / 2) : singlePieceHeight ] = (0, 0, 0)
+		boxWidth = math.floor((singlePieceWidth - nibWidth) / 2)
+		singlePieceImgData[0:nibHeight, 0:boxWidth] = (0, 0, 0)
+		singlePieceImgData[0:nibHeight, singlePieceWidth - boxWidth : singlePieceWidth ] = (0, 0, 0)
 
 		return singlePieceImgData
 
+	def addInnerNib(self, singlePieceImgData, singlePieceDimensions, coords, nibHeight):
+		x, y = coords
+		singlePieceWidth, singlePieceHeight = singlePieceDimensions
+
+		# Use square nibs for now
+		nibWidth = nibHeight
+
+		boxWidth = math.floor((singlePieceWidth - nibWidth) / 2)
+		singlePieceImgData[0:nibHeight, 0:singlePieceWidth] = (0, 0, 0)
+		singlePieceImgData[nibHeight:2 * nibHeight, boxWidth : singlePieceWidth - boxWidth ] = (0, 0, 0)
+
+		return singlePieceImgData
 	def addNibs(self, singlePieceImgData, singlePieceDimensions, coords, nibHeight):
 		x, y = coords
 		singlePieceWidth, singlePieceHeight = singlePieceDimensions
 
-		rotationsForOuterNibsMapping = {
-			Direction.UP :0,
-			Direction.RIGHT :1,
-			Direction.DOWN :2,
-			Direction.LEFT :3			
-		}
-
-		direction = Direction.DOWN
-		# for direction in Direction.GetAllDirections():
-			# if (piecesArray[x][y].getEdgeGeometry(direction) == EdgeShape.OUT):
-		singlePieceImgData = np.rot90(singlePieceImgData, rotationsForOuterNibsMapping[direction])
-		singlePieceImgData = self.addOuterNib(singlePieceImgData, (singlePieceWidth, singlePieceHeight), (x, y), nibHeight)
-		singlePieceImgData = np.rot90(singlePieceImgData, 4 - rotationsForOuterNibsMapping[direction])
+		for direction in Direction.GetAllDirections():
+			if (piecesArray[x][y].getEdgeGeometry(direction) == EdgeShape.OUT):
+				singlePieceImgData = np.rot90(singlePieceImgData, direction.value)
+				singlePieceImgData = self.addOuterNib(singlePieceImgData, (singlePieceWidth, singlePieceHeight), (x, y), nibHeight)
+				singlePieceImgData = np.rot90(singlePieceImgData, 4 - direction.value)
+			elif (piecesArray[x][y].getEdgeGeometry(direction) == EdgeShape.IN):
+				singlePieceImgData = np.rot90(singlePieceImgData, rotation[direction])
+				singlePieceImgData = self.addInnerNib(singlePieceImgData, (singlePieceWidth, singlePieceHeight), (x, y), nibHeight)
+				singlePieceImgData = np.rot90(singlePieceImgData, 4 - rotation[direction])
+		# print(singlePieceImgData)
 
 		return singlePieceImgData
 
 
 	def breakImageToPieces(self, xNumPieces, yNumPieces, imageArray):
-		imgWidth, imgHeight, rgb = imageArray.shape
+		imgHeight, imgWidth, rgb = imageArray.shape
 		nibHeight = math.floor((imgWidth / xNumPieces) * PuzzlePiece.NIB_PERCENT)
 
 		imgSliceWidth = math.floor(imgWidth / xNumPieces) 
@@ -164,10 +162,10 @@ class Puzzle:
 		paddedImageArray = np.pad(imageArray, ((nibHeight, nibHeight), (nibHeight, nibHeight),(0,0)), 'constant')
 		# print(paddedImageArray)
 
+		finalImagePiece = None
 		for x in range(xNumPieces):
+			singlePuzzleCol = None
 			for y in range(yNumPieces):
-				x = 2
-				y = 2
 				x1 = (x * imgSliceWidth) - nibHeight
 				x2 = x1 + singlePieceWidth
 
@@ -177,13 +175,18 @@ class Puzzle:
 				singlePieceImgData = paddedImageArray[y1 + nibHeight : y2 + nibHeight, x1 + nibHeight : x2 + nibHeight]
 
 				singlePieceWithNibsImgData = self.addNibs(singlePieceImgData, (singlePieceWidth, singlePieceHeight), (x, y), nibHeight)
+				paddedImagePiece = np.pad(singlePieceWithNibsImgData, ((nibHeight, nibHeight), (nibHeight, nibHeight),(0,0)), 'constant', constant_value=(255,255,255))
+				np.concatenate((singlePuzzleCol, paddedImagePiece), 0)
+
+			np.concatenate((finalImagePiece, singlePuzzleCol), 0)
+
 				# print(singlePieceWidth)
 				# print(singlePieceHeight)
 				# print(imgData.shape)
 				# piecesArray[x, y].imgData
-				imgDisp = Image.fromarray(singlePieceWithNibsImgData, 'RGB')
-				imgDisp.show()
-				aaa
+		imgDisp = Image.fromarray(finalImagePiece, 'RGB')
+		imgDisp.show()
+		aaa
 
 			# piecesArray[x, y].imgData
 		imgDisp = Image.fromarray(paddedImageArray, 'RGB')
@@ -195,7 +198,7 @@ class Puzzle:
 		im_array = np.array(image)
 
 		# Generate the nibs that we want to use
-		# generatePuzzleGeometry(self.xNumPieces, self.yNumPieces)
+		self.generatePuzzleGeometry(self.xNumPieces, self.yNumPieces)
 
 		# Break the image array into each piece
 		self.breakImageToPieces(self.xNumPieces, self.yNumPieces,im_array)
