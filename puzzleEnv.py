1a0c1140781def116ac208aa259b6f4729afc3d5
Manish Gupta <manigu@microsoft.com>
2018-07-19 17:57:41 -0700

    Got it to run wihtout errors

--- puzzleEnv.py
+++ puzzleEnv.py
@@ -1,6 +1,8 @@
 from enum import Enum
+import numpy as np
+import random
 
-class EdgeShape(enum):
+class EdgeShape(Enum):
 	STRAIGHT = 0
 	IN = 1
 	OUT = 2
@@ -13,34 +15,8 @@ class EdgeShape(enum):
 
 		return IN
 
-class PuzzlePiece:
-	
-	def init(self, imgData, coords):
-		self.imgData = imgData # tbd
-		self.coords = coords
-		self.edgeGeometry = 0 # L, D, R, U (2 bits per direction totals 1 byte)
 
-	def getEdgeGeometry(self, direction):
-		if direction == UP:
-			return EdgeShape(int(bin(edgeGeometry)[2:][6:8]), 2)
-		else if direction == RIGHT:
-			return EdgeShape(int(bin(edgeGeometry)[2:][4:6]), 2)
-		else if direction == DOWN:
-			return EdgeShape(int(bin(edgeGeometry)[2:][2:4]), 2)
-		else if direction == LEFT:
-			return EdgeShape(int(bin(edgeGeometry)[2:][0:2]), 2)
-
-	def setEdgeGeometry(self, direction, edgeShape):
-		if direction == UP:
-			edgeGeometry = edgeGeometry | (edgeShape)
-		else if direction == RIGHT:
-			edgeGeometry = edgeGeometry | (edgeShape << 2)
-		else if direction == DOWN:
-			edgeGeometry = edgeGeometry | (edgeShape << 4)
-		else if direction == LEFT:
-			edgeGeometry = edgeGeometry | (edgeShape << 6)
-
-class Direction(enum):
+class Direction(Enum):
 	UP = 0
 	RIGHT = 1
 	DOWN = 2
@@ -56,23 +32,52 @@ class Direction(enum):
 		if (direction == LEFT):
 			direction = RIGHT
 
-class Puzzle:
-	piecesArray = [][]
 
-	def createEdge(piece0Coords, piece1Coords, p0EdgeDirection):
+class PuzzlePiece:
+	
+	def init(self, imgData, coords):
+		self.imgData = imgData # tbd
+		self.coords = coords
+		self.edgeGeometry = 0 # L, D, R, U (2 bits per direction totals 1 byte)
+
+	def getEdgeGeometry(self, direction):
+		if direction == UP:
+			return EdgeShape(int(bin(edgeGeometry)[2:][6:8]), 2)
+		elif direction == RIGHT:
+			return EdgeShape(int(bin(edgeGeometry)[2:][4:6]), 2)
+		elif direction == DOWN:
+			return EdgeShape(int(bin(edgeGeometry)[2:][2:4]), 2)
+		elif direction == LEFT:
+			return EdgeShape(int(bin(edgeGeometry)[2:][0:2]), 2)
+
+	def setEdgeGeometry(self, direction, edgeShape):
+		if direction == UP:
+			edgeGeometry = edgeGeometry | (edgeShape)
+		elif direction == RIGHT:
+			edgeGeometry = edgeGeometry | (edgeShape << 2)
+		elif direction == DOWN:
+			edgeGeometry = edgeGeometry | (edgeShape << 4)
+		elif direction == LEFT:
+			edgeGeometry = edgeGeometry | (edgeShape << 6)
+
+
+class Puzzle:
+	piecesArray = None
+
+	def createEdge(self, piece0Coords, piece1Coords, p0EdgeDirection):
 		if (piecesArray[piece0Coords].getEdgeGeometry(p0EdgeDirection) != EdgeShape.STRAIGHT):
 			return
 
 		p1EdgeDirection = Direction.GetComplement(p0EdgeDirection)
 
-		p0EdgeShape = EdgeShape(random.rand(1,3))
+		p0EdgeShape = EdgeShape(random.randint(1,2))
 		p1EdgeShape = EdgeShape.GetComplement(p0EdgeShape)
 
 		piecesArray[piece0Coords].setEdgeGeometry(p0EdgeDirection, p0EdgeShape)
 		piecesArray[piece1Coords].setEdgeGeometry(p1EdgeDirection, p1EdgeShape)
 
-	def generatePuzzleGeometry(puzzleWidth, puzzleHeight):
-		self.piecesArray = [puzzleWidth][puzzleHeight]
+	def generatePuzzleGeometry(self, puzzleWidth, puzzleHeight):
+		self.piecesArray = numpy.zeros(shape=(puzzleWidth,puzzleHeight))
 		for x in puzzleWidth:
 			for y in puzzleHeight:
 				piecesArray[x, y].coords = (x, y)
@@ -84,7 +89,17 @@ class Puzzle:
 
 				if (y < (puzzleWidth - 1)):
 					createEdge((x,y), (x,y+1), Direction.DOWN)
+
 				if (y > 0):
 					createEdge((x,y), (x,y-1), Direction.UP)
 
+    # def breakImageToPieces(self, puzzleWidth, puzzleHeight, image):
+    #     generatePuzzleGeometry(puzzleWidth, puzzleHeight)
+ 
 
+def main():
+
+    return
+
+if __name__ == "__main__":
+    main()
