66d5add2a00b895e286c88b5a21b394a67ba50b5
Matthew Bongiovi <mattbo@microsoft.com>
2018-08-30 15:00:10 -0700

    Fix game bugs

--- Environment/Snake/snakeEnvironment.py
+++ Environment/Snake/snakeEnvironment.py
@@ -9,11 +9,10 @@ import numpy as np
 
 
 class Apple:
-    x = 0
-    y = 0
-    step = 44
 
     def __init__(self, x, y):
+        self.step = 50
+
         self.x = x * self.step
         self.y = y * self.step
 
@@ -24,8 +23,8 @@ class Apple:
         counter = 0
         touchesSnake = True
         while counter < 1000000 and touchesSnake:
-            self.x = randint(1, 17) * 44
-            self.y = randint(1, 12) * 44
+            self.x = randint(0, (800 / 50) - 1) * self.step
+            self.y = randint(0, (600 / 50) - 1) * self.step
             touchesSnake = False
 
             for i in range(0, player.length):
@@ -39,7 +38,6 @@ class Apple:
 
 class Player:
 
-    step = 44
     direction = 0
     length = 3
 
@@ -52,13 +50,15 @@ class Player:
         self.y = [0]
 
         self.length = length
+        self.step = 50 
+
         for i in range(0, 2000):
             self.x.append(-100)
             self.y.append(-100)
 
         # initial positions, no collision.
-        self.x[1] = 1*44
-        self.x[2] = 2*44
+        self.x[1] = 1 * self.step
+        self.x[2] = 2 * self.step
 
     def update(self):
 
@@ -110,21 +110,18 @@ class Player:
 
 class Game:
 
-    def isOutOfBounds(self, x1, y1, bsize):
-        if x1 >= 550:
+    def isOutOfBounds(self, x1, y1):
+        if x1 >= 800:
             return True
 
-        if y1 >= 750:
+        if y1 >= 700:
             return True
 
         if x1 < 0 or y1 < 0:
             return True
         return False
 
-    def isCollision(self, x1, y1, x2, y2, bsize):
-        # if x1 >= x2 and x1 <= x2 + bsize:
-        #     if y1 >= y2 and y1 <= y2 + bsize:
-        #         return True
+    def isCollision(self, x1, y1, x2, y2,):
         if x1 == x2 and y1 == y2:
             return True
         return False
@@ -215,18 +212,18 @@ class SnakeEnvironment(Environment):
         done = False
 
         # does snake eat apple?
-        if self.game.isCollision(self.apple.x, self.apple.y, self.player.x[0], self.player.y[0], 44):
+        if self.game.isCollision(self.apple.x, self.apple.y, self.player.x[0], self.player.y[0]):
             self.apple = self.apple.place(self.player)
             self.player.length = self.player.length + 1
             reward = 5
 
         # does snake collide with itself?
         for i in range(2, self.player.length):
-            if self.game.isCollision(self.player.x[0], self.player.y[0], self.player.x[i], self.player.y[i], 40):
+            if self.game.isCollision(self.player.x[0], self.player.y[0], self.player.x[i], self.player.y[i]):
                 reward = -5
                 done = True
 
-        if self.game.isOutOfBounds(self.player.x[0], self.player.y[0], 44):
+        if self.game.isOutOfBounds(self.player.x[0], self.player.y[0]):
             reward = -5
             done = True
 
