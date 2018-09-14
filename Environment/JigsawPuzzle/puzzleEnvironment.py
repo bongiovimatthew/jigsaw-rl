6e2216516b82b4444e1446d846c30dba4eab77d2
Manish Gupta <manigu@microsoft.com>
2018-09-14 12:18:52 -0700

    Actually change reward structure....

--- Environment/JigsawPuzzle/puzzleEnvironment.py
+++ Environment/JigsawPuzzle/puzzleEnvironment.py
@@ -3,7 +3,7 @@ from Environment.JigsawPuzzle.direction import Direction
 from Environment.JigsawPuzzle.edge import EdgeShape
 from Environment.JigsawPuzzle.puzzleFactory import PuzzleFactory
 from enum import Enum
-
+import random
 
 class Actions(Enum):
     ACTION_TRANS_UP    = 0
@@ -58,7 +58,8 @@ class PuzzleEnvironment(Environment):
             count = 0
             for piece in self.pieceState:
                 if (piece.coords_y == after[1]) and piece.coords_x == after[0]:
-                    self.currentPieceIndex = count
+                    cycle_offset = 0 # random.randint(0, 4)
+                    self.currentPieceIndex = count - cycle_offset
 
                 if (self.debugMode):
                     print("piece.guid:{0}, piece.coords_x:{1}, piece.coords_y:{2}".format(
@@ -67,6 +68,8 @@ class PuzzleEnvironment(Environment):
                 count += 1
         else:
             self.currentPieceIndex = 0
+    
+        # self.currentPieceIndex = random.randint(0, len(self.pieceState) - 1) #
 
         self.oldScore = self.getScoreOfCurrentState()
         self.stepCount = 0
@@ -158,7 +161,7 @@ class PuzzleEnvironment(Environment):
         self.stepCount += 1
         next_state = self._convert_state(action)
         currentScore = self.getScoreOfCurrentState()
-        done = self.isMaxReward(currentScore) or (self.stepCount > 5)
+        done = self.isMaxReward(currentScore) or (self.stepCount > 35)
 
         tempOldScore = self.oldScore
         self.oldScore = currentScore
@@ -174,11 +177,11 @@ class PuzzleEnvironment(Environment):
         #     reward = -1
 
         # reward = reward / 180
-        # reward = ((reward / 180) + 1) / 10
+        reward = ((reward / 180)) # / 2
         if self.isMaxReward(currentScore):
-            reward = 1
-        else:
-            reward = 0
+            reward = 2
+        # else:
+        #     reward = 0
 
         self.numberOfTimesExecutedEachAction[action] += 1
 
